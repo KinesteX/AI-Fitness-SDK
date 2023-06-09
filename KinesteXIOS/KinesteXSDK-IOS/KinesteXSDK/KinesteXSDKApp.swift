8438d3b4eb8e17f135a4092200b7f660667792d2
Vladimir Shetnikov <62508191+V-m1r@users.noreply.github.com>
2023-06-08 20:29:36 -0400

    Add files via upload

---
+++ KinesteXIOS/KinesteXSDK-IOS/KinesteXSDK/KinesteXSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinesteXSDKApp.swift
+//  KinesteXSDK
+//
+//  Created by Vladimir Shetnikov on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct KinesteXSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
